23ca7e2b556120b0945e89be81d4d845ec18d6d1
Mauricio Tamayo <cesar.tamayo@turismo.gob.ec>
2025-03-26 16:21:43 -0500

    fix bugs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    API_URL: 'http://api-sis-ecu-enaccion.turismo.gob.ec/api/v1',
+    API_URL: 'https://api-sis-ecu-enaccion.turismo.gob.ec/api/v1',
     APP_NAME: 'Sistema Informático Ecuatorianos en Acción',
     APP_SHORT_NAME: 'SIEEA',
     RECAPTCHA_SITE_KEY: '6Lfw3v8qAAAAANpHVyPuWDDVbCUNryg9OxdfTsGg'
